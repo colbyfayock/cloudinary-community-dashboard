761c44001a915fe8de7be6a8f7a5ea953e00f52f
Colby Fayock <fayock@gmail.com>
2023-06-07 17:26:59 -0400

    youtube top videos

--- src/components/DashboardYouTube.jsx
+++ src/components/DashboardYouTube.jsx
@@ -1,4 +1,16 @@
-import { Card, AreaChart, Text } from "@tremor/react";
+import {
+  AreaChart,
+  Card,
+  List,
+  ListItem,
+  Icon,
+  Text,
+  Bold,
+  Flex,
+  Title,
+  Button,
+  Grid,
+} from "@tremor/react";
 
 import { formatDate } from '../lib/datetime';
 import { sortByDateKey, addCommas } from '../lib/util';
@@ -6,40 +18,69 @@ import { sortByDateKey, addCommas } from '../lib/util';
 const DashboardYouTube = ({ data }) => {
   const reportType = 'daily';
 
-  const reports = Object.keys(data).map(key => {
-    const sortedData = sortByDateKey(data[key][reportType], 'date');
-    return {
-      id: key,
-      title: data[key].title,
-      keys: data[key].keys,
-      data: sortedData.map(data => {
-        return {
-          ...data,
-          date: formatDate(data.date)
-        }
-      })
-    }
-  });
-
   return (
     <>
-      <Card>
-        {reports.map(report => {
+      {Object.entries(data).map(([id, report]) => {
+        if ( report.type === 'chart' ) {
+          const sortedData = {
+            id: id,
+            title: report.title,
+            keys: report.keys,
+            data: sortByDateKey(report[reportType], 'date').map(data => {
+              return {
+                ...data,
+                date: formatDate(data.date)
+              }
+            })
+          }
           return (
-            <div key={report.id}>
+            <Card key={id} className="my-8">
               <Text className="text-md font-semibold">{ report.title }</Text>
               <AreaChart
                 className="mt-4 mb-8 h-60"
-                data={report.data}
+                data={sortedData.data}
                 index="date"
-                categories={report.keys}
+                categories={sortedData.keys}
                 colors={["indigo"]}
                 valueFormatter={(value) => addCommas(value)}
               />
+            </Card>
+          )
+        }
+        if ( report.type === 'list' ) {
+          return (
+            <div key={id}>
+              <Card className="my-8">
+                <Title>{ report.title }</Title>
+                {/* <Text>{item.name}</Text> */}
+                <List className="mt-4">
+                  {report.all.map(({ video, count }) => (
+                    <ListItem key={video.id}>
+                      <Flex justifyContent="start" className="truncate space-x-4">
+                        {/* <Icon
+                          variant="light"
+                          icon={transaction.icon}
+                          size="md"
+                          color={transaction.color}
+                        /> */}
+                        <div className="truncate">
+                          <Text className="truncate">
+                            <Bold>{video.title}</Bold>
+                          </Text>
+                          <Text className="truncate">
+                            <a href={video.url} target="_blank">{ video.url }</a>
+                          </Text>
+                        </div>
+                      </Flex>
+                      <Text>{count}</Text>
+                    </ListItem>
+                  ))}
+                </List>
+              </Card>
             </div>
           )
-        })}
-      </Card>
+        }
+      })}
     </>
   )
 }
