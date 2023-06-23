369755b8b1d20b5df04e2d12e6de3c9232930b4c
Colby Fayock <fayock@gmail.com>
2023-06-23 15:58:15 -0400

    cleanup and prep for grouping

--- src/lib/datetime.js
+++ src/lib/datetime.js
@@ -1,6 +1,6 @@
 export function formatDate(dateString) {
   const date = new Date(dateString);
-  const month = date.getMonth();
+  const month = date.getMonth() + 1;
   const day = date.getDate();
   const year = date.getFullYear();
   return `${month}/${day}/${year}`;
