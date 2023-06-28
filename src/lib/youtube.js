21562cee08750d23edf3550320e2f775de91127b
Colby Fayock <fayock@gmail.com>
2023-06-28 12:55:47 -0400

    revertting

--- src/lib/youtube.js
+++ src/lib/youtube.js
@@ -55,80 +55,43 @@ const ECOSYSTEM_TAGS = [
 ]
 
 /**
- * getTotalViews
+ * getTotalByType
  */
 
-function getTotalViews(videos) {
-  return videos.reduce((prev, curr) => {
+export function getTotalByType({ data, key, type, intervalType }) {
+	const dataOfType = data.filter(d => d.type === intervalType);
+  const lastMonthIndex = dataOfType.length - 1 - 28;
+  const dataCurrent = dataOfType[dataOfType.length - 1];
+  const dataLastMonth = dataOfType[lastMonthIndex];
+
+  const totalViews = dataCurrent.data.reduce((prev, curr) => {
     return prev + curr.views.count;
   }, 0);
-}
 
-/**
- * getTotalViewsByDate
- */
+  let videos;
+  let total = '';
 
-function getTotalViewsByDate(videos) {
-  return videos.map(({ date, data }) => {
-		const counts = data.map((d) => d.views.count);
+  if ( type === 'all' ) {
+    videos = dataOfType;
+    total = totalViews;
+  } else if ( type === 'lastMonth' ) {
+    videos = dataOfType.slice(lastMonthIndex, dataOfType.length - 1)
+
+    const lastMonthViews = dataLastMonth.data.reduce((prev, curr) => {
+      return prev + curr.views.count;
+    }, 0);
+
+    total = totalViews - lastMonthViews;
+  }
+
+  const totals = videos.map(({ date, data }) => {
+		const counts = data.map((d) => d[key].count);
 		const total = counts.reduce((prev, curr) => prev + curr)
 		return {
 			date: formatDate(date),
-			views: total
+			[key]: total
 		}
 	});
-}
-
-/**
- * getTotalsByType
- */
-
-export function getTotalsByType({ data, type, intervalType }) {
-	const dataOfType = data.filter(d => d.type === intervalType);
-  const lastMonthIndex = dataOfType.length - 1 - 28;
-  const dataLastMonth = dataOfType[lastMonthIndex];
-
-  // const groupedTotals = dataOfType.map(item => {
-  //   return {
-  //     data: groupVideosByEcosystemTag(item.data),
-  //     date: item.date
-  //   }
-  // });
-  
-  let totals = getTotalViewsByDate(dataOfType);
-
-  const totalsByDate = Object.entries(test).reduce((prev, [key, value]) => {
-    value.forEach(item => {
-      if ( !prev[item.date] ) {
-        prev[item.date] = 0;
-      }
-
-      prev[item.date] = prev[item.date] + item.views;
-    })
-
-    return prev;
-  }, {});
-
-  let totals = Object.keys(totalsByDate).map(key => ({
-    date: key,
-    views: totalsByDate[key]
-  }))
-
-  // With the totals broken down by date, the last entry
-  // will have the total alltime views
-
-  const allTimeViews = totals[totals.length - 1].views;
-
-  let total = allTimeViews;
-
-  // If we're not dealing with alltime values, we need to modify
-  // the dataset and recalculate the total number we're returning
-
-  if ( type === 'lastMonth' ) {
-    totals = totals.slice(lastMonthIndex, dataOfType.length);
-    const lastMonthViews = getTotalViews(dataLastMonth.data);
-    total = allTimeViews - lastMonthViews;
-  }
 
 	return {
     total,
@@ -137,10 +100,10 @@ export function getTotalsByType({ data, type, intervalType }) {
 }
 
 /**
- * getViewsByVideo
+ * getTotalViewsByVideo
  */
 
-export function getViewsByVideo({ data, type }) {
+export function getTotalViewsByVideo({ data, type }) {
   let videos;
 
 	if ( type === 'all' ) {
@@ -151,7 +114,7 @@ export function getViewsByVideo({ data, type }) {
 			}
 		})
 	} else if ( type === 'lastMonth' ) {
-    const lastMonthIndex = data.length - 28;
+    const lastMonthIndex = data.length - 1 - 28;
     const dataLastMonth = data[lastMonthIndex];
     const dataCurrent = data[data.length - 1];
 
@@ -192,17 +155,12 @@ export function getViewsByVideo({ data, type }) {
   const formattedData = sortedData.map(data => {
     return {
       ...data,
-      value: data.value
-      // value: addCommas(data.value)
+      value: addCommas(data.value)
     }
   });
 		
 	return formattedData;
 }
-
-/**
- * getEcosytemTagsFromVideoTags
- */
 
 function getEcosytemTagsFromVideoTags(videoTags) {
   const tags = [];
@@ -220,24 +178,4 @@ function getEcosytemTagsFromVideoTags(videoTags) {
   return Array.from(new Set(tags)).map(tag => {
     return ECOSYSTEM_TAGS.find(({ name }) => name === tag)
   });
-}
-
-/**
- * groupVideosByEcosystemTag
- */
-
-function groupVideosByEcosystemTag(videos) {
-  const tagGroups = {};
-
-  videos.forEach(video => {
-    const ecosystemTags = getEcosytemTagsFromVideoTags(video.tags);
-    ecosystemTags.forEach(tag => {
-      if ( !tagGroups[tag.name] ) {
-        tagGroups[tag.name] = {};
-      }
-      tagGroups[tag.name][video.id] = video;
-    })
-  });
-
-  return tagGroups;
 }
